bb804c8d571918a27f1185c16fa657c8dd1d9109
Lisa Bamberg <lisambamberg@gmail.com>
2018-04-16 22:31:05 -0500

    Complete Project

---
+++ RefactorKata/Product.cs
@@ -0,0 +1,8 @@
+﻿namespace RefactorKata
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        
+    }
+}
--- RefactorKata/Program.cs
+++ RefactorKata/Program.cs
@@ -8,30 +8,31 @@ namespace RefactorKata
     {
         static void Main(string[] args)
         {
-            var conn = new SqlConnection("Server=.;Database=myDataBase;User Id=myUsername;Password = myPassword;");
+            List<Product> products = new List<Product>();
 
-            SqlCommand cmd = conn.CreateCommand();
-            cmd.CommandText = "select * from Products";
-
-            var reader = cmd.ExecuteReader();
-            var products = new List<Product>();
-
-            while (reader.Read())
+            using (var conn = new SqlConnection("Server = .; " +
+     "Database = myDataBase; " +
+     "User Id = myUsername; " +
+     "Password = myPassword;"))
             {
-                var prod = new Product { name = reader["Name"].ToString()};
-                products.Add(prod);
+                var cmd = conn.CreateCommand();
+                cmd.CommandText = "select * from Products";
+
+                var reader = cmd.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    products.Add(new Product { Name = reader["Name"].ToString() });
+                }
+
             }
-            conn.Dispose();
+
             Console.WriteLine("Products Loaded!");
-            for (int i =0; i< products.Count; i++)
+
+            foreach (var product in products)
             {
-                Console.WriteLine(products[i].name);
+                Console.WriteLine(product.Name);
             }
         }
-    }
-    public class Product
-    {
-        public string name;
-        public string Name { get { return name; } set { name = value; } }
     }
 }
