a44bfb6f1b4dddd764b8ee8a9099f733f3f53b9f
Lisa Bamberg <lisambamberg@gmail.com>
2018-04-16 21:44:41 -0500

    Initial Commit

--- RefactorKata/Program.cs
+++ RefactorKata/Program.cs
@@ -8,9 +8,9 @@ namespace RefactorKata
         static void Main(string[] args)
         {
             //This is intentionally bad : (  Let's Refactor!
-            System.Data.SqlClient.SqlConnection Conn = new System.Data.SqlClient.SqlConnection("Server=.;Database=myDataBase;User Id=myUsername;Password = myPassword;");
+            var conn = new System.Data.SqlClient.SqlConnection("Server=.;Database=myDataBase;User Id=myUsername;Password = myPassword;");
 
-            System.Data.SqlClient.SqlCommand cmd = Conn.CreateCommand();
+            System.Data.SqlClient.SqlCommand cmd = conn.CreateCommand();
             cmd.CommandText = "select * from Products";
             /*
              * cmd.CommandText = "Select * from Invoices";
@@ -25,7 +25,7 @@ namespace RefactorKata
                 prod.name = reader["Name"].ToString();
                 products.Add(prod);
             }
-            Conn.Dispose();
+            conn.Dispose();
             Console.WriteLine("Products Loaded!");
             for (int i =0; i< products.Count; i++)
             {
